dd9a4811e6cd9306b06b89dc0fa9c19363286de5
Derrick J. Wippler <thrawn01@gmail.com>
2011-10-26 15:43:47 -0500

    Applied what was learned from the performance tests

--- tests/test_AsyncClient.py
+++ tests/test_AsyncClient.py
@@ -56,48 +56,42 @@ class AsyncClient(object):
     
     def __init__(self):
         pass
+
+    def log(self, msg):
+        sys.stderr.write(msg)
     
     def parseURI(self,uri):
         url = urlparse(uri)
-        if not 'http' in url[0]:
-            raise RuntimeError("Only (http,https) supported")
-
         port = int(url.port or 80)
         path = url.path or '/'
+        scheme = url[0]
+
+        if not 'http' in scheme:
+            raise RuntimeError("Only (http,https) supported")
+
         # ( scheme, host, port, path )
-        return (url[0],url.hostname, port, path )
+        return (scheme, url.hostname, port, path)
 
-    def get_slow(self, url):
-        url = UrlParse(url)
-        return self._request_header(url.host(), url.path(), (('Host',url.host()),('Scheme',url.scheme())))
-
-    def get_fast(self, url):
-        (scheme, host, port, path) = self.parseURI(url)
-        return self._request_header(host, path, (('Host',host),('Scheme',scheme)))
-
-    def get(self,url):
-        url = UrlParse(url)
-        self.log( "Connecting %s:%s ..." % (url.host(), url.port()) )
-        # Connect to the remote Host
-        sock = socket.create_connection((url.host(), url.port()))
-        sock.send(self._request_header('GET', url.path(), (('Host',url.host()),)))
-        return sock
-   
     def _request_header(self, method, path='/', headers=(), version='HTTP/1.1'):
         headers = [": ".join(x) for x in headers]
         return "%s %s %s\r\n%s\r\n\r\n" % (method, path, version,"\r\n".join(headers))
-         
-        
-    def log(self, msg):
-        sys.stderr.write(msg)
 
+    def get(self,url):
+        # Parse the URI
+        (scheme, host, port, path) = self.parseURI(url)
+        # Connect to the remote Host
+        sock = socket.create_connection((host, port))
+        # Sending the request header
+        sock.send(self._request_header('GET', path, (('Host',host),)))
+        return sock
+   
 
 class TestAsyncClient(TestCase):
 
     @staticmethod
-    def yield_app(env, start_response):
+    def chunked_app(env, start_response):
         start_response('200 OK', [('Content-Type', 'text/plain')])
-        yield ['hello world']
+        yield 'hello world'
 
     @staticmethod
     def application(env, start_response):
@@ -105,7 +99,7 @@ class TestAsyncClient(TestCase):
         return ['hello world']
 
     def setUp(self):
-        self.application = validator(self.yield_app)
+        self.application = validator(self.application)
         self.server = pywsgi.WSGIServer(('127.0.0.1', 15001), self.application)
         self.server.start()
         self.port = self.server.server_port
@@ -132,32 +126,4 @@ class TestAsyncClient(TestCase):
 
         #sock.send('GET / HTTP/1.1\r\nHost: localhost\r\n\r\n')
         print sock.recv(1500)
-
-
-if __name__ == '__main__':
-    client = AsyncClient()
-
-    # K Class, what did we learn today
-    # == Object access is slow ==
-
-    # This is SLOW
-    def port(self): 
-        if self.url.port:
-            return int(self.url.port)
-        return int(80)
-    
-    # This is FAST ( But ugly )
-    def port(self): 
-        port = self.url.port
-        if port:
-            return int(port)
-        return int(80)
-
-    # get_fast is faster because it avoids all the method calls and object access
-    for i in range(1,1000000):
-        value = client.get_fast('http://127.0.0.1:15001')
-        #value = client.get_slow('http://127.0.0.1:15001')
-    
-    print value
-
 
