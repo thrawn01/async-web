47a766a16a839912d2554e2b85713b3f876ad58f
Derrick J. Wippler <thrawn01@gmail.com>
2011-12-20 16:18:40 -0600

    Separated websocket tests from http server and client tests

--- tests/test_http.py
+++ tests/test_http.py
@@ -252,32 +252,3 @@ class TestAsyncServer(TestCase):
         self.stop(server)
 
 
-    def web_socket_handler(self, request, response):
-        response.write("hello world")
-
-
-    def test_websocket(self):
-        web_socket = WebSocket()
-        web_socket.add('/', self.web_socket_handler)
-
-        ## Create a simple server with a WebSocket filter that responds with 'Hello World'
-        server = AsyncServer(('127.0.0.1', 15001), (web_socket,))
-        server.start()
-
-        #try:
-        socket = AsyncClient().websocket('http://127.0.0.1:15001/')
-
-        self.assertEquals(socket.headers['connection'], 'Upgrade')
-        self.assertEquals(socket.headers['upgrade'], 'websocket')
-        self.assertTrue('date' in socket.headers)
-        self.assertTrue('sec-websocket-accept' in socket.headers)
-        self.assertEquals(socket.status, 101)
-        self.assertEquals(socket.reason, 'Switching Protocols')
-        self.assertEquals(socket.read(),'hello world')
-
-        #except HTTPException, e:
-            #print "HTTP Exception Caught - Status: %s Reason: %s" % (e.status, e.reason)
-
-        self.stop(server)
-
-    
