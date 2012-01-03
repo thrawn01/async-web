68bf08170db8c1ed46a1f28ec68606ada5610716
Derrick J. Wippler <thrawn01@gmail.com>
2012-01-03 16:47:44 -0600

    Removed the need for external websocket python library

--- tests/test_websocket.py
+++ tests/test_websocket.py
@@ -3,47 +3,6 @@ from asyncweb import AsyncClient, AsyncServer
 from unittest import TestCase
 import gevent
 
-# XXX: Remove later
-from websocket.server import WebSocketServer
-
-
-class TestAsyncClientWebSocket(TestCase):
-    
-    @staticmethod
-    def application(environ, start_response):
-        if 'wsgi.get_websocket' not in environ:
-            start_response('400 Not WebSocket', [])
-            return []
-        websocket = environ['wsgi.get_websocket']()
-        websocket.do_handshake()
-        while True:
-            message = websocket.receive()
-            if message is None:
-                break
-            websocket.send('echo' + message)
-   
-
-    def setUp(self):
-        self.server = WebSocketServer(('127.0.0.1', 15001), self.application, policy_server=False)
-        self.server.start()
-        self.port = self.server.server_port
-
-
-    def tearDown(self):
-        timeout = gevent.Timeout(0.5,RuntimeError("Timeout trying to stop server"))
-        timeout.start()
-        try:
-            self.server.stop()
-        finally: timeout.cancel()
-
-
-    #def test_connect(self):
-    #    try:
-    #        resp = AsyncClient().get('http://127.0.0.1:15001/')
-    #        print resp.read()
-    #    except HTTPException, e:
-    #        print "Caught Exception: %s, %s" % (e.status, e.reason)
-
 
 class TestWebSocketServer(TestCase):
 
